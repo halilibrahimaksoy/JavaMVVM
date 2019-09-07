4de3cd9232004320f9fb0ad3b6406905140ac452
halilibrahimaksoy <Aksoy.6193>
2019-09-07 14:30:47 +0300

    first commit

---
+++ app/src/main/java/com/haksoy/veloxitydemo/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.haksoy.veloxitydemo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.haksoy.veloxitydemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
